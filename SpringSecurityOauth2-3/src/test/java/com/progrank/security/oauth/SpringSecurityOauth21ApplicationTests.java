045b4ccd4fbb6333f71ee81f4e99a6469b723cb4
ajit51 <68638451+ajit51@users.noreply.github.com>
2022-02-16 17:02:42 +0530

    Add files via upload

---
+++ SpringSecurityOauth2-3/src/test/java/com/progrank/security/oauth/SpringSecurityOauth21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progrank.security.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauth21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
