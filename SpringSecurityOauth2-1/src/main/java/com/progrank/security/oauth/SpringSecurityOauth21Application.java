dbb3b5b4c682ea695348217c049a57da6ae8eea3
ajit51 <68638451+ajit51@users.noreply.github.com>
2022-02-16 17:01:44 +0530

    Add files via upload

---
+++ SpringSecurityOauth2-1/src/main/java/com/progrank/security/oauth/SpringSecurityOauth21Application.java
@@ -0,0 +1,13 @@
+package com.progrank.security.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauth21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOauth21Application.class, args);
+	}
+
+}
